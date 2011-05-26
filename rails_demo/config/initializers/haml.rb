c3baaa1abb7e0b340404153857effd1a3392bf61
Ben Langfeld <ben@langfeld.me>
2011-05-26 15:40:19 +0100

    Add rails demo skeleton

---
+++ rails_demo/config/initializers/haml.rb
@@ -0,0 +1,3 @@
+    Haml::Template.options[:attr_wrapper] = '"'
+    Haml::Template.options[:format] = :html5
+    Sass::Plugin.options[:style] = :expanded
---
+++ rails_demo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsIntegrationDemo::Application.config.secret_token = '8a246ebbb52b682788c6a1583268d271f0ad7321690ce79b4049e8bae0d83e4bd96547a9436e1aabe13de9ae55dbf682437b1d27c82984ebf7f6cceae6406a5c'
