23ed075a9976899b3da571bcd26edf5c40c35ea7
Ben Langfeld <ben@langfeld.me>
2011-05-29 14:50:22 +0200

    Shorter conference name

--- demo_app/app/dialplan.rb
+++ demo_app/app/dialplan.rb
@@ -2,7 +2,7 @@ tropo_agi {
   participant_id = call.variables['participant_id'] || 1
   participant = Contact.find participant_id
   play "I'm going to drop you in to the conference call now."
-  join 'theconference'
+  join 'conference'
 }
 
 redirect {
